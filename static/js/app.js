c1d411fb4c6255060df89c95ed25a01a8b979671
okaokaw <okaokaw@126.com>
2020-06-02 23:39:31 +0800

    0.3

--- static/js/app.js
+++ static/js/app.js
@@ -3,7 +3,7 @@ var canvas = document.querySelector("canvas");
 
 
 //手写板对象
-var hwPad = new SignaturePad(canvas, {
+var signaturePad = new SignaturePad(canvas, {
   backgroundColor: 'rgb(255,255,255)', // 画白色背景，否则默认透明或者黑色，笔也是黑色，出来就是全黑
   minWidth: 3,
   maxWidth: 3
@@ -11,27 +11,66 @@ var hwPad = new SignaturePad(canvas, {
 
 
 //实时响应
-hwPad.onEnd = function() {
+signaturePad.onEnd = function() {
     send_img();
+
 };
 
 
-//按比例缩放，改变窗口大小时会清除
+//按比例缩放
 function resizeCanvas() {
-  var ratio =  Math.max(window.devicePixelRatio || 1, 1);
-  canvas.width = canvas.offsetWidth;
-  canvas.height = canvas.offsetHeight;
-  //canvas.getContext("2d").scale(ratio, ratio);
-  hwPad.clear();
+        // When zoomed out to less than 100%, for some very strange reason,
+        // some browsers report devicePixelRatio as less than 1
+        // and only part of the canvas is cleared then.
+        var context = canvas.getContext("2d"); //context.getImageData(0,0,canvas.width,canvas.height)
+        var imgData = signaturePad ? signaturePad.toData() : null;
+        var ratio =  Math.max(window.devicePixelRatio || 1, 1);
+        canvas.width = canvas.offsetWidth * ratio;
+        canvas.height = canvas.offsetHeight * ratio;
+        context.scale(ratio, ratio);
+        // context.putImageData(imgData,0,0);
+        imgData && signaturePad.fromData(imgData);
 }
 
 window.onresize = resizeCanvas;
 resizeCanvas();
 
+//点击复制 LaTeX 代码
+function copy_text(){
+    var clipboard = new ClipboardJS('#pad-result', {
+        target: function() {
+            return document.querySelector('#pad-result');
+        }
+    });
+}
+document.getElementById('pad-result').onclick = copy_text();
+
+
+//点击复制渲染图片
+function copy_latex_img() {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){}
+
+    else {
+        html2canvas(document.getElementById('Latex')).then(function (canvas) {
+            canvas.toBlob(function (blob) {
+                const item = new ClipboardItem({"image/png": blob});
+                navigator.clipboard.write([item]);
+            });
+            console.log(canvas.toDataURL('image/png'));
+        });
+    }
+}
+document.getElementById('Latex').addEventListener('click', function() {
+    copy_latex_img();
+});
+
+
+
+
 
 //给后端服务器传图片信息
 function send_img() {
-    imgData = {URL: hwPad.toDataURL().substring(22)}; //JSON
+    imgData = {URL: signaturePad.toDataURL().substring(22)}; //JSON
 
     //用 request 与后端通信, POST
     var request = new XMLHttpRequest();
@@ -45,7 +84,18 @@ function send_img() {
                   katex.render(request.response, document.getElementById('Latex'), {
                         throwOnError: false
                   });
-              };
+/*
+                  //转换为canvas
+                  html2canvas(document.getElementById('Latex')).then(function(canvas) {
+                      $('#Latex').empty();
+                        var image = new Image();
+                        image.id = "Latex_img";
+                        image.src = canvas.toDataURL();
+                        document.getElementById('Latex').appendChild(image);
+                  });
+*/
+
+              }
           }
       };
     request.open("POST", "./predict");
@@ -59,7 +109,7 @@ function send_img() {
 //按钮事件绑定
 //清除按钮
 document.getElementById('pad-clear').addEventListener("click", function (event) {
-  hwPad.clear();
+  signaturePad.clear();
   document.querySelector('#pad-result').innerHTML="";
   katex.render("", document.getElementById('Latex'), {
       throwOnError: false
@@ -69,7 +119,7 @@ document.getElementById('pad-clear').addEventListener("click", function (event) 
 /*
 //识别按钮
 document.getElementById('pad-predict').addEventListener("click", function (event) {
-  if (hwPad.isEmpty()) {
+  if (signaturePad.isEmpty()) {
     alert("请书写一个数字");
   } else {
      send_img();
@@ -79,17 +129,18 @@ document.getElementById('pad-predict').addEventListener("click", function (event
 
 //橡皮擦按钮
 document.getElementById('pad-erase').addEventListener('click', function () {
-  //hwPad.dotSize = 20;
-    hwPad.penColor = "rgb(255,255,255)";
-    hwPad.minWidth = 20;
-    hwPad.maxWidth = 20;
+  //signaturePad.dotSize = 20;
+    signaturePad.penColor = "rgb(255,255,255)";
+    signaturePad.minWidth = 20;
+    signaturePad.maxWidth = 20;
 });
 
 //画笔按钮
 document.getElementById('pad-draw').addEventListener('click', function () {
-    //hwPad.lineWidth = 10;
-    hwPad.penColor = "rgb(0,0,0)";
-    hwPad.minWidth = 3;
-    hwPad.maxWidth = 3;
+    //signaturePad.lineWidth = 10;
+    signaturePad.penColor = "rgb(0,0,0)";
+    signaturePad.minWidth = 3;
+    signaturePad.maxWidth = 3;
 });
+
 
